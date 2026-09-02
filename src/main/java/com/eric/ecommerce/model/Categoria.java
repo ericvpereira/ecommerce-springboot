d496a3da263133703abc2e17748a2ed0024dfa3f
Eric Vieira <ericvieira.p@outlook.com>
2026-09-02 20:05:09 -0300

    feat: configure JPA entity mapping

--- src/main/java/com/eric/ecommerce/model/Categoria.java
+++ src/main/java/com/eric/ecommerce/model/Categoria.java
@@ -1,14 +1,17 @@
 package com.eric.ecommerce.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Categoria {
-	
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String nome;
 
 	public Categoria(Integer id, String nome) {
--- src/main/java/com/eric/ecommerce/model/Product.java
+++ src/main/java/com/eric/ecommerce/model/Product.java
@@ -3,18 +3,23 @@ package com.eric.ecommerce.model;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Product {
-	
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
-	
+
 	private String nome;
 	private BigDecimal preco;
 
