af61ce4d708f5139845b7d9954e0f7899e36eabd
Eric Vieira <ericvieira.p@outlook.com>
2026-09-09 20:54:55 -0300

    feat: add product validation

--- src/main/java/com/eric/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/eric/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package com.eric.ecommerce.exceptions;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +18,20 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex) {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+		Map<String, String> map = new HashMap<>();
+		List<FieldError> erros = ex.getBindingResult().getFieldErrors();
+
+		for (FieldError erro : erros) {
+			map.put(erro.getField(), erro.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
 
 	}
 
